3e0155e97e34d7c5ebf35c7a90d33aed0a12cfa6
James Hrivnak <James.Hrivnak@united.com>
2023-12-15 15:33:15 -0700

    create table with athleteId=partitionKey, activityId=sortKey

--- data_utilities.py
+++ data_utilities.py
@@ -4,6 +4,7 @@ import requests
 from pprint import pprint
 from auth_utilities import get_access_token_from_athlete_id
 from flask import Blueprint, make_response, request, jsonify
+from decimal import Decimal
 
 data_controller_bp = Blueprint('data_controller', __name__)
 
@@ -40,20 +41,36 @@ def route_fetch_all_activities():
       print("Exception when calling ActivitiesApi -> getLoggedInAthleteActivities: %s\n" % e)
       return ('Fetch All Activities Error: %s\n' % e)
 
-def fetch_all_activities_req(access_token, page):
+def fetch_all_activities_strava_req(access_token, page):
     url = "https://www.strava.com/api/v3/activities"
     r = requests.get(url + '?access_token=' + access_token, params={ 'page': page, 'per_page': 200 })
     r = r.json()
 
     if len(r) == 200:
-      return r + fetch_all_activities_req(access_token, page + 1)
+      return r + fetch_all_activities_strava_req(access_token, page + 1)
     return r
+
+def fetch_all_activities_req(srg_athlete_id):
+  dynamodb = boto3.resource('dynamodb')
+  activities_table = dynamodb.Table('srg-activities-table')
+
+  response = activities_table.query(
+      KeyConditionExpression="#athlete_id = :athlete_id",
+      ExpressionAttributeNames={
+          "#athlete_id": "athleteId",
+      },
+      ExpressionAttributeValues={
+          ":athlete_id": srg_athlete_id,
+      }
+  )
+
+  return response['Items']
+
 
 
 def fetch_all_activities():
   srg_athlete_id = request.args.get('srg_athlete_id')
-  access_token = get_access_token_from_athlete_id(srg_athlete_id)
-  r = fetch_all_activities_req(access_token, 1)
+  r = fetch_all_activities_req(srg_athlete_id)
   return json.dumps(r)
 
 ###### Get Logged In User ######
@@ -94,5 +111,73 @@ def fetch_athlete_stats_req(athleteId, access_token):
    r = r.json()
    return r
 
+###### Add All Activities ######
+
+@data_controller_bp.route('/srg/addAllActivities', methods=["POST"])
+def route_add_all_activities():
+  return add_all_activities()
+
+def add_all_activities():
+   srg_athlete_id = request.args.get('srg_athlete_id')
+   access_token = get_access_token_from_athlete_id(srg_athlete_id)
+
+   activities_to_return = add_all_activities_req(access_token)
+
+   return activities_to_return
+
+def add_all_activities_req(access_token):
+  activities_to_add = fetch_all_activities_req(access_token, 3)
+  # Sort and Filter Activities
+  activities_to_add = sorted(activities_to_add, key=lambda x: x['distance'] / x['moving_time'], reverse=True)
+  activities_to_add = list(filter(lambda x: x['type'] in ["Walk", "Swim", "Run", "Ride"], activities_to_add))
+
+  # Upload Activities To DynamoDB
+  activities_to_add = [
+    {
+       'athleteId': str(entry['athlete']['id']),
+       'activityId': str(entry['id']),
+       'name': entry['name'],
+       'type': entry['type'],
+       'start_date': entry['start_date'],
+       'distance': Decimal(str(entry['distance'])),
+       'moving_time': entry['moving_time'],
+       'elapsed_time': entry['elapsed_time'],
+       'average_speed': Decimal(str(entry['average_speed'])),
+       'max_speed': Decimal(str(entry['max_speed'])),
+       'elev_high': Decimal(str(entry['elev_high'])) if 'elev_high' in entry else None,
+       'elev_low': Decimal(str(entry['elev_low'])) if 'elev_low' in entry else None,
+       'total_elevation_gain': Decimal(str(entry['total_elevation_gain'])),
+       'average_heartrate': Decimal(str(entry['average_heartrate'])) if 'average_heartrate' in entry else None,
+       'max_heartrate': Decimal(str(entry['max_heartrate'])) if 'max_heartrate' in entry else None,
+       'location_city': entry['location_city'],
+       'location_state': entry['location_state'],
+       'location_country': entry['location_country'],
+       'achievement_count': entry['achievement_count'],
+       'kudos_count': entry['kudos_count'],
+       'comment_count': entry['comment_count'],
+       'pr_count': entry['pr_count']
+    } for entry in activities_to_add
+  ]
+
+  dynamodb = boto3.resource('dynamodb')
+  activities_table = dynamodb.Table('srg-activities-table')
+
+  with activities_table.batch_writer() as batch:
+      for activity in activities_to_add:
+          batch.put_item(Item=activity)
+
+  return activities_to_add;
 
 
+
+
+
+
+
+      #  'location_city': entry['location_city'],
+      #  'location_state': entry['location_state'],
+      #  'location_country': entry['location_country'],
+      #  'achievement_count': entry['achievement_count'],
+      #  'kudos_count': entry['kudos_count'],
+      #  'comment_count': entry['comment_count'],
+      #  'pr_count': entry['pr_count']
