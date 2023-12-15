bb7e2a0379359553af5ba3079c9d1e30b8855b17
James Hrivnak <James.Hrivnak@united.com>
2023-12-14 19:12:53 -0700

    update to new ALB

--- iac/lib/srg-python-stack.ts
+++ iac/lib/srg-python-stack.ts
@@ -94,15 +94,6 @@ export class SRGPythonStack extends cdk.Stack {
           streamPrefix: 'srgp-container',
           logRetention: RetentionDays.FIVE_DAYS,
         }),
-        // healthCheck: {
-        //   command: [
-        //     'CMD-SHELL',
-        //     'curl -f http://127.0.0.1:5000/healthcheck || exit 1',
-        //   ],
-        //   interval: cdk.Duration.seconds(30),
-        //   timeout: cdk.Duration.seconds(10),
-        //   retries: 5,
-        // },
       }
     );
 
@@ -111,41 +102,41 @@ export class SRGPythonStack extends cdk.Stack {
       hostPort: 5000,
     });
 
-    // const albListener = ApplicationListener.fromLookup(
-    //   this,
-    //   'imported-listener',
-    //   {
-    //     listenerArn:
-    //       'arn:aws:elasticloadbalancing:us-east-1:471507967541:listener/app/jh-alb/5927623bf7b387b8/202d118fecee2aa5',
-    //   }
-    // );
+    const importedALBListener = ApplicationListener.fromLookup(
+      this,
+      'imported-listener',
+      {
+        listenerArn:
+          'arn:aws:elasticloadbalancing:us-east-1:471507967541:listener/app/jh-alb/5927623bf7b387b8/202d118fecee2aa5',
+      }
+    );
 
-    // const targetGroup = new ApplicationTargetGroup(this, 'srg-python-tg', {
-    //   // targetGroupName: 'srg-svc-target',
-    //   port: 5000,
-    //   protocol: ApplicationProtocol.HTTP,
-    //   targets: [srgFargateService],
-    //   vpc: ec2.Vpc.fromLookup(this, 'jh-imported-vpc-tg', {
-    //     vpcId: props.aws_env.AWS_VPC_ID,
-    //   }),
-    //   healthCheck: {
-    //     path: '/srg/healthcheck',
-    //     unhealthyThresholdCount: 2,
-    //     healthyHttpCodes: '200',
-    //     healthyThresholdCount: 5,
-    //     interval: cdk.Duration.seconds(30),
-    //     port: '5000',
-    //     timeout: cdk.Duration.seconds(10),
-    //   },
-    // });
+    const targetGroup = new ApplicationTargetGroup(this, 'srg-python-tg', {
+      // targetGroupName: 'srg-svc-target',
+      port: 5000,
+      protocol: ApplicationProtocol.HTTP,
+      targets: [srgFargateService],
+      vpc: ec2.Vpc.fromLookup(this, 'jh-imported-vpc-tg', {
+        vpcId: props.aws_env.AWS_VPC_ID,
+      }),
+      healthCheck: {
+        path: '/srg/healthcheck',
+        unhealthyThresholdCount: 2,
+        healthyHttpCodes: '200',
+        healthyThresholdCount: 5,
+        interval: cdk.Duration.seconds(30),
+        port: '5000',
+        timeout: cdk.Duration.seconds(10),
+      },
+    });
 
-    // albListener.addTargetGroups('srg-listener-tg', {
-    //   targetGroups: [targetGroup],
-    //   priority: 20,
-    //   conditions: [
-    //     aws_elasticloadbalancingv2.ListenerCondition.pathPatterns(['/srg/*']),
-    //   ],
-    // });
+    importedALBListener.addTargetGroups('srg-listener-tg', {
+      targetGroups: [targetGroup],
+      priority: 20,
+      conditions: [
+        aws_elasticloadbalancingv2.ListenerCondition.pathPatterns(['/srg/*']),
+      ],
+    });
 
     new dynamodb.Table(this, 'srg-token-table', {
       tableName: 'srg-token-table',
